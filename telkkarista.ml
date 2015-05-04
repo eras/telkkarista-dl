440ce2526c08b1b95ef362396311aaf7909c2fc1
Erkki Seppälä <flux@modeemi.fi>
2015-05-05 00:12:43 +0300

    Telkkarista: better handling of environment

--- telkkarista.ml
+++ telkkarista.ml
@@ -9,8 +9,9 @@ let return = Lwt.return
 
 let (/^) a b = a ^ "/" ^ b
 
-type common = {
+type 'env common = {
   c_session : string;
+  c_env     : [`Environment];
 }
 
 module API =
@@ -84,7 +85,7 @@ let request endpoint request from_json common respond =
   | `Ok response ->
     respond (Some response) 
 
-let checkSession env common =
+let checkSession common =
   Lwt_unix.run (
     (* ~body:(`Stream (Lwt_stream.of_list ["foo"])) *)
     let%lwt response = get ~session:common.c_session ~endpoint:Endpoints.checkSession in
@@ -102,12 +103,12 @@ let checkSession env common =
       return ()
   )
 
-let checkSession' env common =
+let checkSession' common =
   Lwt_unix.run (
     request Endpoints.checkSession None API.checkSession_response_of_yojson common (fun _ -> return ())
   )
 
-let login env common email password =
+let login common email password =
   Lwt_unix.run (
     let body = Yojson.Safe.to_string @@ API.login_request_to_yojson { API.email; password } in
     let%lwt response = post_without_session ~endpoint:Endpoints.login ~body in
@@ -125,15 +126,16 @@ let login env common email password =
       return ()
   )
 
-let common_opts session = { c_session = session }
+let common_opts env session = { c_session = session;
+                                c_env = env }
 
-let common_opts_t =
+let common_opts_t env =
   let docs = "COMMON OPTIONS" in 
   let session = 
     let doc = "Set session id" in
     Arg.(value & opt string "" & info ["s"; "session"] ~docs ~doc)
   in
-  Term.(pure common_opts $ session)
+  Term.(pure (common_opts env) $ session)
 
 let username_t =
   let doc = "User name (email)" in
@@ -150,20 +152,20 @@ let help_subcommands = [
   `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command.";
   `S "BUGS"; `P "Check bug reports at http://bugs.example.org.";]
 
-let default_prompt = 
+let default_prompt env = 
   let doc = "An unofficial standalone command line client for Telkkarista.com" in 
   let man = help_subcommands in
-  Term.(ret (pure (fun _ -> `Help (`Pager, None)) $ common_opts_t)),
+  Term.(ret (pure (fun _ -> `Help (`Pager, None)) $ common_opts_t env)),
   Term.info program_name ~version ~sdocs:"COMMON OPTIONS" ~doc ~man
 
 let cmd_checkSession env =
   let doc = "Check session status" in
-  Term.(pure (checkSession env) $ common_opts_t),
+  Term.(pure checkSession $ common_opts_t env),
   Term.info "checkSession" ~version ~doc
 
 let cmd_login env =
   let doc = "Log into the service" in
-  Term.(pure (login env) $ common_opts_t $ username_t $ password_t),
+  Term.(pure login $ common_opts_t env $ username_t $ password_t),
   Term.info "login" ~version ~doc
 
 let main () =
@@ -171,8 +173,8 @@ let main () =
     cmd_checkSession;
     cmd_login;
   ] in
-  let env = () in
-  match Term.eval_choice default_prompt (List.map (fun x -> x env) subcommands)
+  let env = `Environment in
+  match Term.eval_choice (default_prompt env) (List.map (fun x -> x env) subcommands)
   with
   | `Error _ -> exit 1
   | _        -> exit 0
