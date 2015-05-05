d505946b4d9268c996100fd0a09dc35b35664bca
Erkki Seppälä <flux@modeemi.fi>
2015-05-05 23:22:51 +0300

    Tools: added some comments

--- tools.ml
+++ tools.ml
@@ -1,9 +1,12 @@
-let partition_map p xs =
+(* Partitions a list in two parts depending on the criteria returned by predicate.
+
+   Compared to List.partition this also maps each element produced to the lists. *)
+let partition_map : ('a -> [< `Left of 'b | `Right of 'c ]) -> 'a list -> 'b list * 'c list  = fun predicate xs ->
   let rec loop xs left right =
     match xs with
     | [] -> (List.rev left, List.rev right)
     | x::xs ->
-      match p x with
+      match predicate x with
       | `Left x -> loop xs (x::left) (right)
       | `Right x -> loop xs (left) (x::right)
   in
